1565aca89dfe20ef67ad87c0e8b6b0837351767b
unknown <anjaliantil.antil@gmail.com>
2019-09-05 22:46:45 +0530

    webtube

---
+++ public/individual.js
@@ -0,0 +1,53 @@
+var video_id="-G7bJVAIiEI";
+$.ajax({
+    url: "https://www.googleapis.com/youtube/v3/videos?id="+ video_id+"&key=AIzaSyAHbAycr4wdQv4kKznGKUowXHiK65ocV3E" + "&part=contentDetails%2Csnippet%2Cstatistics",
+    dataType: "json",
+    type:'GET',
+    success: function(data){
+             console.log(data.items[0].snippet.title);
+             console.log(data.items[0].statistics.viewCount); 
+             $('#div1').append(data.items[0].snippet.title); 
+             $('#description').append(data.items[0].snippet.description);   
+             $('#views').append(data.items[0].statistics.viewCount);
+             $('#likes').append(data.items[0].statistics.likeCount);
+             $('#dislikes').append(data.items[0].statistics.dislikeCount);
+    },
+    error: function( textStatus, errorThrown) {
+        alert (textStatus, + ' | ' + errorThrown);
+    }
+});
+var obj = {"video": {
+    "value": "<iframe class='embed-responsive-item' width='560' height='315' src='https://www.youtube.com/embed/'"+video_id+ "allowfullscreen></iframe>"}
+}
+    $("#div").html(obj.video.value);
+
+    $.ajax({
+        url: "https://www.googleapis.com/youtube/v3/search?key=AIzaSyAHbAycr4wdQv4kKznGKUowXHiK65ocV3E&part=snippet&relatedToVideoId=-G7bJVAIiEI&type=video&maxResults=10",
+        dataType: "json",
+        type:'GET',
+        success: function(data){
+            for(i=0;i<data.items.length;i++){
+            $('#div3').append("<div id='div4'class='card mb-4' style='max-width: 600x;border-color:white;'"+
+           "<div class='row no-gutters'>"+
+              "<div id='img' class='col-6'>"+
+                 "<img src="+data.items[i].snippet.thumbnails.default.url+"width='230' height='200'class='card-img'>"+"</div>"+
+
+                
+                  "<a href='/video/"+data.items[i].id.videoId+"'"+"><h5 class='title ml-auto;'>"+data.items[i].snippet.title+"</h5></a>"+
+                  "<p id='thumbnail' class='text' style='ml-auto;'></p>"+
+                
+              "</div>"+
+            "</div>"+
+          "</div>")
+                 }
+        }
+    });
+// like button
+    var btn = document.getElementById("btn");
+ var counter = document.getElementById("btn-counter");
+ counter.innerHTML = 0;
+ $('#btn').on('click',function(){
+ 
+    counter.innerHTML++;
+ });
+  
