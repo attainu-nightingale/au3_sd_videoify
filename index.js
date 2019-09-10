86c7a9eb5ba4f99199f603e7fed2cf1d00c83f2c
sagar kumar <kumarsagarmz@gmail.com>
2019-09-10 13:42:42 +0530

    done

--- index.js
+++ index.js
@@ -1,38 +1,36 @@
-var express=require('express');
-var app=express();
+var express = require('express');
+var profile = require('./profileRouter')
+
+var app = express();
 var hbs = require('hbs');
 app.set('view engine', 'hbs');
 app.use(express.static('public'));
-app.get('/',function(req,res){
-    res.sendFile(__dirname+'/public/login.html')
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/public/login.html')
 });
 
-app.get('/home',function(req,res){
-    res.render('home.hbs',{
-        title:'HOME',
-        style:'home.css'
+app.get('/home', function (req, res) {
+    res.render('home.hbs', {
+        title: 'HOME',
+        style: 'home.css'
     })
 });
 
-app.get('/profile',function(req,res){
-        res.render('profile.hbs',{
-            title:'PROFILE',
-            style:'profile.css'
-        })
+app.use('/profile', profile)
+
+app.get('/trending', function (req, res) {
+    res.render('trending.hbs', {
+        title: 'TRENDING',
+        style: 'trending.css'
+    })
 });
 
-app.get('/trending',function(req,res){
-        res.render('trending.hbs',{
-            title:'TRENDING',
-            style:'trending.css'
-        })
-});
-
-app.get('/video/:id',function(req,res){
-    res.render('individual.hbs',{
-            title:'INDIVIDUAL PAGE',
-            style:'individual.css'
-        })
+app.get('/video/:id', function (req, res) {
+    res.render('individual.hbs', {
+        title: 'INDIVIDUAL PAGE',
+        style: 'individual.css'
+    })
 });
 
 app.listen(3000);
