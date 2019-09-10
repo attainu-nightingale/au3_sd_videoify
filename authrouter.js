7a825bee4d5d77f93855ec5db1e8e88cdca2b170
prashant singh <prashantsj643@gmail.com>
2019-09-11 01:48:18 +0530

    done

--- authrouter.js
+++ authrouter.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var session = require ("express-session");
+var session = require("express-session");
 var MongoClient = require('mongodb').MongoClient;
 
 var url = 'mongodb+srv://sagar:kumar@cluster0-ralg6.mongodb.net/webTubeDB?retryWrites=true&w=majority';
 var db;
 
-MongoClient.connect(url, { useUnifiedTopology: true,useNewUrlParser: true }, function (error, client) {
+MongoClient.connect(url, { useUnifiedTopology: true, useNewUrlParser: true }, function (error, client) {
     if (error)
         throw error;
 
@@ -18,6 +18,7 @@ var bodyParser = require("body-parser");
 router.use(
     session({
         secret: "express session secret"
+
     })
 );
 
@@ -27,35 +28,58 @@ router.use(express.static("public"));
 router.use(bodyParser.json());
 
 
+
 router.get("/signup", function (req, res) {
     res.sendFile(__dirname + "/public/signup.html")
 })
 
 router.get("/forgot", function (req, res) {
-    res.sendFile(__dirname + "/public/forgot.html")
+    res.sendFile(__dirname + "/public/forgot1.html")
 })
 
-router.post("/check",function(req,res){
-    db.collection('loginData').find().toArray(function(error,result){
-        if(error) throw error;
-        
-        for(var i=0;i<result.length; i++){
-            
-            if(req.body.email == result[i].email && req.body.password == result[i].password){
+router.post("/check", function (req, res) {
+    db.collection('loginData').find().toArray(function (error, result) {
+        if (error) throw error;
+
+        for (var i = 0; i < result.length; i++) {
+
+            if (req.body.email == result[i].email && req.body.password == result[i].password) {
                 req.session.loggedIn = true;
             }
-            
+
         }
         res.redirect("/authrouter/user")
     });
-    
+
 
 })
 
-router.get("/user",function(req,res){
-    if(req.session.loggedIn == true){
-        res.sendFile(__dirname + "/public/forgot.html")
-    } else{
+router.post("/checkemail", function (req, res) {
+    db.collection('loginData').find().toArray(function (error, result) {
+        if (error) throw error;
+        var counter;
+        for (var i = 0; i < result.length; i++) {
+
+            if (req.body.email == result[i].email) {
+                counter = 1;
+            }
+
+        }
+        if (counter == 1) {
+            res.sendFile(__dirname + "/public/forgot.html")
+        }
+        else {
+            res.send("email address not found");
+        }
+
+
+    })
+})
+
+router.get("/user", function (req, res) {
+    if (req.session.loggedIn == true) {
+        res.sendFile(__dirname + "")
+    } else {
         res.redirect("/");
     }
 });
@@ -63,9 +87,44 @@ router.get("/user",function(req,res){
 
 router.post("/adduser", function (req, res) {
     db.collection('loginData').insertOne(req.body);
-    console.log(req.body)
-    
+    res.send("done");
 })
+
+
+router.post("/validation", function (req, res) {
+    db.collection('loginData').find({}).toArray(function (error, result) {
+        if (error) throw error;
+        var ecounter = 0;
+        var ucounter = 0;
+        for (var i = 0; i < result.length; i++) {
+
+            if (req.body.email == result[i].email) {
+                ecounter++;
+                break;
+            }
+            if (req.body.username == result[i].userName) {
+                ucounter++;
+                break;
+            }
+
+
+        }
+        if (ecounter == 1 && ucounter == 1) {
+            res.send("11")
+        }
+        else if (ecounter == 1) {
+            res.send("5");
+        }
+        else if (ucounter == 1) {
+            res.send("4");
+        }
+        else
+            res.send("0");
+
+
+    })
+
+});
 
 
 
